d3cffa900d8cf229a8b9b39bd4bf21b99e7fb9ff
Mario <Mario>
2015-08-13 22:29:13 +0200

    - initial

---
+++ TGAThumbnailViewer/Readme.txt
@@ -0,0 +1,9 @@
+TGA thumbnail viewer.
+
+License: MIT
+
+Credits:
+
+- Targa Image Reader which is licensed under CPOL license: http://www.codeproject.com/Articles/31702/NET-Targa-Image-Reader
+- SharpShell which is licensed under MIT license: https://github.com/dwmkerr/sharpshell
+- SharpShell code project article: http://www.codeproject.com/Articles/563114/NET-Shell-Extensions-Shell-Thumbnail-Handlers
---
+++ TGAThumbnailViewer/TGAThumbnailHandler.cs
@@ -0,0 +1,28 @@
+﻿// ***********************************************************************
+// Assembly         : TGAThumbnailViewer
+// Author           : Mario
+// Created          : 08-13-2015
+//
+// Last Modified By : Mario
+// Last Modified On : 08-13-2015
+// ***********************************************************************
+// <copyright file="TGAThumbnailHandler.cs" company="">
+//     Copyright (c) . All rights reserved.
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+
+using System;
+
+/// <summary>
+/// The TGAThumbnailViewer namespace.
+/// </summary>
+namespace TGAThumbnailViewer
+{
+    /// <summary>
+    /// Class TGAThumbnailHandler.
+    /// </summary>
+    public class TGAThumbnailHandler
+    {
+    }
+}
